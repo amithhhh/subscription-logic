01c3a291320df85403cd591e03ab053b531c1091
amithhhhh <amithajith7025@gmail.com>
2026-02-10 21:14:26 +0630

    created structure of Plan schema

---
+++ src/models/Plan.mjs
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const planSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        durationInDays: {
+            type: Number,
+            required: true
+        }, 
+        isActive: {
+            type: Boolean,
+            default: true
+        }
+    },
+    {timestamps: true}
+)
+
+const Plan = mongoose.model("Plan", planSchema)
+
+export default Plan
